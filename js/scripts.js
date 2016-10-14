013a31bf5b8d31010f8aa6ff485c4407ea1c2611
Alice Frumin <alifrumin@yahoo.com>
2016-10-14 00:28:53 -0400

    script for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $(function(){
+  //select the first iframe that has a src that starts with "//www.youtube"
+  var firstIframe = document.querySelector('iframe[src^="https://www.youtube"]');
+  //get the current source
+  var src = firstIframe.src;
+  //update the src with "autoplay=1"
+  var newSrc = src+'?autoplay=1';
+  //change iframe's src
+  firstIframe.src = newSrc;
+
   $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
 
   // If you want to keep full screen on window resize
